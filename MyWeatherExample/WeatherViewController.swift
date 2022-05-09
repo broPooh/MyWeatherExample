12ac42250402e8c0e593cda9361f8eec03771b0b
broMac <yhs4705@naver.com>
2022-05-09 17:10:29 +0900

    화면 작성

---
+++ MyWeatherExample/WeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherViewController.swift
+//  MyWeatherExample
+//
+//  Created by bro on 2022/05/09.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var tempTextView: UITextView!
+    @IBOutlet weak var humidityTextView: UITextView!
+    @IBOutlet weak var windSpeedTextView: UITextView!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var messageTextView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
